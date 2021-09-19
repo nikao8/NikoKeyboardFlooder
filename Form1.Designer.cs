3d7dec439a42f34818cf0bd67ff03c2cb09be4a1
Nicolas Marques <nicolasmarquesjf@hotmail.com>
2021-09-19 13:26:54 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace ProjetoFlooderKeyboard
+{
+    partial class FormFlooder
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFlooder));
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioOn = new System.Windows.Forms.RadioButton();
+            this.radioOff = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboboxKey = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 176);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            // 
+            // radioOn
+            // 
+            this.radioOn.AutoSize = true;
+            this.radioOn.Location = new System.Drawing.Point(14, 91);
+            this.radioOn.Name = "radioOn";
+            this.radioOn.Size = new System.Drawing.Size(44, 21);
+            this.radioOn.TabIndex = 15;
+            this.radioOn.TabStop = true;
+            this.radioOn.Text = "On";
+            this.radioOn.UseVisualStyleBackColor = true;
+            // 
+            // radioOff
+            // 
+            this.radioOff.AutoSize = true;
+            this.radioOff.Location = new System.Drawing.Point(64, 91);
+            this.radioOff.Name = "radioOff";
+            this.radioOff.Size = new System.Drawing.Size(44, 21);
+            this.radioOff.TabIndex = 16;
+            this.radioOff.TabStop = true;
+            this.radioOff.Text = "Off";
+            this.radioOff.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(293, 10);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Texto a ser enviado:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecione uma tecla para atalho do flood:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(296, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(205, 96);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleDescription = "";
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboboxKey);
+            this.panel1.Controls.Add(this.radioOff);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.radioOn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(514, 152);
+            this.panel1.TabIndex = 13;
+            this.panel1.Tag = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(280, 14);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "OBS: Para enviar em caixa alta bastar ligar o caps look";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(359, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "DEV: github.com/nikao8";
+            // 
+            // comboboxKey
+            // 
+            this.comboboxKey.FormattingEnabled = true;
+            this.comboboxKey.Items.AddRange(new object[] {
+            "F1",
+            "F2",
+            "F3",
+            "F4",
+            "F5",
+            "F6",
+            "F7",
+            "F8",
+            "F9",
+            "F10",
+            "F11"});
+            this.comboboxKey.Location = new System.Drawing.Point(14, 44);
+            this.comboboxKey.Name = "comboboxKey";
+            this.comboboxKey.Size = new System.Drawing.Size(94, 25);
+            this.comboboxKey.TabIndex = 15;
+            // 
+            // FormFlooder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 159);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormFlooder";
+            this.Text = "Niko Keyboard Flooder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioOn;
+        private System.Windows.Forms.RadioButton radioOff;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboboxKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace ProjetoFlooderKeyboard
+{
+
+    public partial class FormFlooder : Form
+    {
+        
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        protected override void WndProc(ref Message m)
+        {
+
+            // Desabilitadas na leitura de keys
+            const int MYACTION_HOTKEY_ID_INSERT = 13;
+            const int MYACTION_HOTKEY_ID_DELETE = 14;
+            const int MYACTION_HOTKEY_ID_RETURN = 15;
+            const int MYACTION_HOTKEY_ID_END = 16;
+            const int MYACTION_HOTKEY_ID_F12 = 12;
+
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_INSERT, 0, (int)Keys.Insert);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_DELETE, 0, (int)Keys.Delete);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_RETURN, 0, (int)Keys.Return);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_END, 0, (int)Keys.End);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F12, 0, (int)Keys.F12);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID_RETURN);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID_INSERT);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID_DELETE);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID_END);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F12);
+
+            // Habilitadas na leitura
+            const int MYACTION_HOTKEY_ID_F1 = 1;
+            const int MYACTION_HOTKEY_ID_F2 = 2;
+            const int MYACTION_HOTKEY_ID_F3 = 3;
+            const int MYACTION_HOTKEY_ID_F4 = 4;
+            const int MYACTION_HOTKEY_ID_F5 = 5;
+            const int MYACTION_HOTKEY_ID_F6 = 6;
+            const int MYACTION_HOTKEY_ID_F7 = 7;
+            const int MYACTION_HOTKEY_ID_F8 = 8;
+            const int MYACTION_HOTKEY_ID_F9 = 9;
+            const int MYACTION_HOTKEY_ID_F10 = 10;
+            const int MYACTION_HOTKEY_ID_F11 = 11;
+            
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F1, 0, (int)Keys.F1);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F2, 0, (int)Keys.F2);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F3, 0, (int)Keys.F3);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F4, 0, (int)Keys.F4);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F5, 0, (int)Keys.F5);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F6, 0, (int)Keys.F6);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F7, 0, (int)Keys.F7);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F8, 0, (int)Keys.F8);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F9, 0, (int)Keys.F9);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F10, 0, (int)Keys.F10);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID_F11, 0, (int)Keys.F11);
+            
+
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F1)
+            {
+                if(comboboxKey.Text == "F1" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");                  
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F2)
+            {
+                if (comboboxKey.Text == "F2" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F3)
+            {
+                if (comboboxKey.Text == "F3" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F4)
+            {
+                if (comboboxKey.Text == "F4" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F5)
+            {
+                if (comboboxKey.Text == "F5" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F6)
+            {
+                if (comboboxKey.Text == "F6" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F7)
+            {
+                if (comboboxKey.Text == "F7" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F8)
+            {
+                if (comboboxKey.Text == "F8" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F9)
+            {
+                if (comboboxKey.Text == "F9" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F10)
+            {
+                if (comboboxKey.Text == "F10" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID_F11)
+            {
+                if (comboboxKey.Text == "F11" && radioOn.Checked == true)
+                {
+                    SendKeys.Send(richTextBox1.Text);
+                    SendKeys.Send("{ENTER}");
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        public FormFlooder()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desenvolvido por: github.com/nikao8\n\n\nDonate?, chave pix: nicolasmarquesjf@hotmail.com", "Suporte");
+            radioOff.Checked = true;
+        }
+    }
+}
